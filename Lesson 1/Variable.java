5875ad1399c67aacb41a8aadb45c07dd62ddcff4
Giss12 <36027903+Giss12@users.noreply.github.com>
2018-12-21 17:18:37 +0300

    Update Variable.java

--- Lesson 1/Variable.java
+++ Lesson 1/Variable.java
@@ -6,11 +6,11 @@ public class Variable{
 	short ram = 8192;
 	int videoadapter = 70000;
 	double frequance = 2.6;
-
+	}
  	System.out.println("threads 16");
  	System.out.println("intel core i7");
     	System.out.println("ram - 8Gb");
     	System.out.println("1080ti");
     	System.out.println("2.634 Ghz");   
  	}
-      }
+      
