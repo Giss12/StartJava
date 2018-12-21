49644a1d9af162bb809560cb669686fd0b60ea34
Giss12 <36027903+Giss12@users.noreply.github.com>
2018-12-21 11:17:54 +0300

    Update Variable.java

--- Lesson 1/Variable.java
+++ Lesson 1/Variable.java
@@ -1,16 +1,16 @@
 
 public class Variable{
  	public static void main(String[] args) {
-		byte threads = 16;
-		boolean proccessor = true;
-		short ram = 8192;
-		int videoadapter = 70000;
-		double frequance = 2.6;
+	byte threads = 16;
+	boolean proccessor = true;
+	short ram = 8192;
+	int videoadapter = 70000;
+	double frequance = 2.6;
 
- 		threads = 16;
- 		System.out.println("threads 16");
- 		proccessor = true;
- 		System.out.println("intel core i7");
+ 	threads = 16;
+ 	System.out.println("threads 16");
+ 	proccessor = true;
+ 	System.out.println("intel core i7");
     	ram = 8192;
     	System.out.println("ram - 8Gb");
     	videoadapter = 70000;
