04264862dab46a78d8ce824cf26b43e44a8ea3ec
Giss12 <36027903+Giss12@users.noreply.github.com>
2018-12-21 11:05:00 +0300

    Delete Unicode.java

--- Unicode.java
+++
@@ -1,8 +0,0 @@
-public class Unicode {
-    public static void main (String [] args) {
-
-    	for (char symbolNumber = 33; symbolNumber <= 126; symbolNumber++) {
-        	System.out.println(symbolNumber);
-    	}
-    }
-}
