de823d4ffcf038a3a824567be4a70bf246092046
Giss12 <36027903+Giss12@users.noreply.github.com>
2018-12-21 16:07:42 +0300

    Update Variable.java

--- Lesson 1/Variable.java
+++ Lesson 1/Variable.java
@@ -7,15 +7,10 @@ public class Variable{
 	int videoadapter = 70000;
 	double frequance = 2.6;
 
- 	threads = 16;
  	System.out.println("threads 16");
- 	proccessor = true;
  	System.out.println("intel core i7");
-    	ram = 8192;
     	System.out.println("ram - 8Gb");
-    	videoadapter = 70000;
     	System.out.println("1080ti");
-    	frequance = 2.6;
     	System.out.println("2.634 Ghz");   
- }
-}
+ 	}
+      }
